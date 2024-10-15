b2b2b41b76d85504aae478bbe2023aeef705b123
Robert Taylor <robert.taylor426@gmail.com>
2024-10-15 16:07:10 +0100

    feat: add configuration for more agenda cards

--- client/src/agendaCards.ts
+++ client/src/agendaCards.ts
@@ -4,6 +4,7 @@ import archivedSecret from './assets/agendaCards/archived-secret.webp';
 import armedForcesStandardization from './assets/agendaCards/armed-forces-standardization.webp';
 import armsReduction from './assets/agendaCards/arms-reduction.webp';
 import checksAndBalances from './assets/agendaCards/checks-and-balances.webp';
+import colonialRedistribution from './assets/agendaCards/colonial-redistribution.webp';
 import committeeFormation from './assets/agendaCards/committee-formation.webp';
 import conventionsOfWar from './assets/agendaCards/conventions-of-war.webp';
 import covertLegislation from './assets/agendaCards/covert-legislation.webp';
@@ -16,12 +17,14 @@ import ixthianArtifact from './assets/agendaCards/ixthian-artifact.webp';
 import judicialAbolishment from './assets/agendaCards/judicial-abolishment.webp';
 import ministerOfWar from './assets/agendaCards/minister-of-war.webp';
 import miscountDisclosed from './assets/agendaCards/miscount-disclosed.webp';
+import mutiny from './assets/agendaCards/mutiny.webp';
 import newConstitution from './assets/agendaCards/new-constitution.webp';
 import nexusSovereignty from './assets/agendaCards/nexus-sovereignty.webp';
 import prophecyOfIxth from './assets/agendaCards/prophecy-of-ixth.webp';
 import rearmamentAgreement from './assets/agendaCards/rearmament-agreement.webp';
 import regulatedConscription from './assets/agendaCards/regulated-conscription.webp';
 import seedsOfAnEmpire from './assets/agendaCards/seed-of-an-empire.webp';
+import wormholeResearch from './assets/agendaCards/wormhole-research.webp';
 
 const agendaCards = [
     'Anti-Intellectual Revolution',
@@ -29,6 +32,7 @@ const agendaCards = [
     'Armed Forces Standardization',
     'Arms Reduction',
     'Checks and Balances',
+    'Colonial Redistribution',
     'Committee Formation',
     'Conventions of War',
     'Covert Legislation',
@@ -41,12 +45,14 @@ const agendaCards = [
     'Judicial Abolishment',
     'Minister of War',
     'Miscount Disclosed',
+    'Mutiny',
     'New Constitution',
     'Nexus Sovereignty',
     'Prophecy of Ixth',
     'Rearmament Agreement',
     'Regulated Conscription',
     'Seed of an Empire',
+    'Wormhole Research',
 ] as const;
 
 type AgendaCard = (typeof agendaCards)[number];
@@ -57,6 +63,7 @@ const agendaCardFaces: Record<AgendaCard, string> = {
     'Armed Forces Standardization': armedForcesStandardization,
     'Arms Reduction': armsReduction,
     'Checks and Balances': checksAndBalances,
+    'Colonial Redistribution': colonialRedistribution,
     'Committee Formation': committeeFormation,
     'Conventions of War': conventionsOfWar,
     'Covert Legislation': covertLegislation,
@@ -69,12 +76,14 @@ const agendaCardFaces: Record<AgendaCard, string> = {
     'Judicial Abolishment': judicialAbolishment,
     'Minister of War': ministerOfWar,
     'Miscount Disclosed': miscountDisclosed,
+    Mutiny: mutiny,
     'New Constitution': newConstitution,
     'Nexus Sovereignty': nexusSovereignty,
     'Prophecy of Ixth': prophecyOfIxth,
     'Rearmament Agreement': rearmamentAgreement,
     'Regulated Conscription': regulatedConscription,
     'Seed of an Empire': seedsOfAnEmpire,
+    'Wormhole Research': wormholeResearch,
 };
 
 export { AgendaCard, agendaCardBack, agendaCardFaces, agendaCards };
